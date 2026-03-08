aa8d8ea85c3800eb640a78c5209060932615326c
ImadSalamah <eamdasalamah@gmail.com>
2026-03-09 01:26:34 +0200

    last of the selector 3 lecture

--- cypress/e2e/selectors-2.cy.js
+++ cypress/e2e/selectors-2.cy.js
@@ -4,6 +4,16 @@ describe('selectors examples', () => {
     it('find the username field by using his contain such as "username"', () => {
         cy.visit('/')
         cy.contains('secret_sauce')
+        // if there are multiple elements with the same contain then it will select the first one and it may not be the one we want to select so we can use .first() to select the first one and .last() to select the last one and .eq(index) to select the element at the index
+
+
     });
+       // and we can use find() to find the element inside another element such as cy.find('div').contains('secret_sauce') it will find the div element that contains the text secret_sauce and then it will find the element inside that div element
+       it('find div contains se', () => {
+        cy.visit('/')
+        cy.get('div').find("div").contains('secret_sauce')
+        cy.get('[data-test="username"]').type('secret_sauce')   
+        
+       });
     
 });
